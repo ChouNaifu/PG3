3b561c6de7ab4bfb8fc10a7cf6b703bb20d41840
ChouNaifu <k024g1023@g.neec.ac.jp>
2025-05-30 15:23:15 +0900

    5/30

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -9,13 +9,13 @@ private:
 
 public:
     MinClass(T1 a, T2 b) : x(a), y(b) {}
-
-    auto Min() const -> decltype(x < y ? x : y) {
+	// Function to find the minimum of x and y
+    auto Min() const {
         return (x < y) ? x : y;
     }
 
     void Print() const {
-        
+		// Print the types and values of x and y
         if (sizeof(x) == sizeof(int) && sizeof(y) == sizeof(int))
             printf("Min(%d, %d) = %d\n", (int)x, (int)y, (int)Min());
         else if (sizeof(x) == sizeof(float) || sizeof(y) == sizeof(float))
@@ -28,7 +28,7 @@ public:
 
 //main function
 int main() {
-
+	//int, float, double 三種類型のMinClassを作成し、各型のインスタンスを生成して最小値を表示する
     MinClass<int, int> a(3, 7);           a.Print();
     MinClass<int, float> b(3, 7.5f);      b.Print();
     MinClass<int, double> c(3, 7.5);      c.Print();
