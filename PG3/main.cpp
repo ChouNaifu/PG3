0fc399899cc3d8464dffd62f52734348020bc9ea
ChouNaifu <k024g1023@g.neec.ac.jp>
2025-07-11 15:55:15 +0900

    7/11

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,81 +1,50 @@
 #include<stdio.h>
-#include<list>
-
-// 駅名をchar配列で定義（英語表記のみ）
-const char* yamanote_1970[][2] = {
-    {"Tabata", "Tabata"},
-    {"Nishi-Nippori", "Nishi-Nippori"}, // 1971年開業
-    {"Nippori", "Nippori"},
-    {"Uguisudani", "Uguisudani"},
-    {"Ueno", "Ueno"},
-    {"Okachimachi", "Okachimachi"},
-    {"Akihabara", "Akihabara"},
-    {"Kanda", "Kanda"},
-    {"Tokyo", "Tokyo"},
-    {"Yurakucho", "Yurakucho"},
-    {"Shimbashi", "Shimbashi"},
-    {"Hamamatsucho", "Hamamatsucho"},
-    {"Tamachi", "Tamachi"},
-    {"Shinagawa", "Shinagawa"},
-    {"Osaki", "Osaki"},
-    {"Gotanda", "Gotanda"},
-    {"Meguro", "Meguro"},
-    {"Ebisu", "Ebisu"},
-    {"Shibuya", "Shibuya"},
-    {"Harajuku", "Harajuku"},
-    {"Yoyogi", "Yoyogi"},
-    {"Shinjuku", "Shinjuku"},
-    {"Shin-Okubo", "Shin-Okubo"},
-    {"Takadanobaba", "Takadanobaba"},
-    {"Mejiro", "Mejiro"},
-    {"Ikebukuro", "Ikebukuro"},
-    {"Otsuka", "Otsuka"},
-    {"Sugamo", "Sugamo"},
-    {"Komagome", "Komagome"},
-    {"Tabata", "Tabata"}
-};
-
-// 2019年: 高輪ゲートウェイ駅(2020年開業)を追加
-const char* takanawa = "Takanawa Gateway";
-const char* nishinippori = "Nishi-Nippori";
-
-void print_list(std::list<const char*>& lst) {
-    for (std::list<const char*>::iterator it = lst.begin(); it != lst.end(); ++it) {
-        printf("%s\n", *it);
-    }
-    printf("\n");
-}
+#include<vector>
+#include<string>
+#include<algorithm>
 
 int main() {
-    // 1970年
-    printf("【1970年】\n");
-    std::list<const char*> list1970;
-    for (int i = 0; i < 30; ++i) {
-        // 1971年開業の西日暮里駅は1970年には含めない
-        if (i == 1) continue;
-        list1970.push_back(yamanote_1970[i][0]);
-    }
-    print_list(list1970);
+    // メールアドレス配列（vector<string>で宣言）
+    std::vector<std::string> numbers = {
+        "k022g0108@g.neec.ac.jp","k022g0045@g.neec.ac.jp","k022g0007@g.neec.ac.jp","k022g0015@g.neec.ac.jp",
+        "k022g0028@g.neec.ac.jp","k022g0049@g.neec.ac.jp","k022g0003@g.neec.ac.jp","k022g0044@g.neec.ac.jp",
+        "k022g0072@g.neec.ac.jp","k022g0055@g.neec.ac.jp","k021g1139@g.neec.ac.jp","k021g1485@g.neec.ac.jp",
+        "k021g1162@g.neec.ac.jp","k021g1278@g.neec.ac.jp","k020g1229@g.neec.ac.jp","k020g1202@g.neec.ac.jp",
+        "k020g1215@g.neec.ac.jp","k020g1416@g.neec.ac.jp","k020g1245@g.neec.ac.jp","k020g1506@g.neec.ac.jp",
+        "k020g1401@g.neec.ac.jp","k022g0089@g.neec.ac.jp","k022g0107@g.neec.ac.jp","k022g0135@g.neec.ac.jp",
+        "k022g0020@g.neec.ac.jp","k022g0046@g.neec.ac.jp","k022g0126@g.neec.ac.jp","k022g0068@g.neec.ac.jp",
+        "k022g1060@g.neec.ac.jp","k022g0019@g.neec.ac.jp","k022g0013@g.neec.ac.jp","k022g0024@g.neec.ac.jp",
+        "k022g0134@g.neec.ac.jp","k022g0123@g.neec.ac.jp","k022g0030@g.neec.ac.jp","k022g0053@g.neec.ac.jp",
+        "k022g0023@g.neec.ac.jp","k022g0041@g.neec.ac.jp","k022g0063@g.neec.ac.jp","k022g0059@g.neec.ac.jp",
+        "k022g0078@g.neec.ac.jp","k022g0011@g.neec.ac.jp","k022g0040@g.neec.ac.jp","k022g0088@g.neec.ac.jp",
+        "k022g0099@g.neec.ac.jp","k022g0060@g.neec.ac.jp","k022g0032@g.neec.ac.jp","k022g0087@g.neec.ac.jp",
+        "k022g0016@g.neec.ac.jp","k022g0116@g.neec.ac.jp","k022g0056@g.neec.ac.jp","k022g0009@g.neec.ac.jp",
+        "k022g0034@g.neec.ac.jp","k022g0062@g.neec.ac.jp","k022g0102@g.neec.ac.jp","k022g0113@g.neec.ac.jp",
+        "k022g0064@g.neec.ac.jp","k022g0080@g.neec.ac.jp","k022g0114@g.neec.ac.jp","k022g0025@g.neec.ac.jp",
+        "k022g0095@g.neec.ac.jp","k022g0121@g.neec.ac.jp","k022g0021@g.neec.ac.jp","k022g0104@g.neec.ac.jp",
+        "k022g0101@g.neec.ac.jp","k022g0130@g.neec.ac.jp","k022g0036@g.neec.ac.jp","k022g0004@g.neec.ac.jp",
+        "k022g0093@g.neec.ac.jp","k022g0038@g.neec.ac.jp","k022g0069@g.neec.ac.jp","k022g0029@g.neec.ac.jp",
+        "k022g0010@g.neec.ac.jp","k022g0083@g.neec.ac.jp","k022g0086@g.neec.ac.jp","k022g0129@g.neec.ac.jp",
+        "k022g0048@g.neec.ac.jp","k022g0027@g.neec.ac.jp","k022g0103@g.neec.ac.jp","k022g0117@g.neec.ac.jp",
+        "k022g0085@g.neec.ac.jp","k022g0052@g.neec.ac.jp","k022g0073@g.neec.ac.jp","k022g0065@g.neec.ac.jp",
+        "k022g0091@g.neec.ac.jp","k022g0136@g.neec.ac.jp","k022g0079@g.neec.ac.jp","k022g0017@g.neec.ac.jp",
+        "k022g0097@g.neec.ac.jp","k022g0077@g.neec.ac.jp","k022g0050@g.neec.ac.jp","k022g0033@g.neec.ac.jp",
+        "k022g0039@g.neec.ac.jp","k022g0118@g.neec.ac.jp","k022g0112@g.neec.ac.jp","k022g0120@g.neec.ac.jp",
+        "k022g0127@g.neec.ac.jp","k021g1464@g.neec.ac.jp","k022g0137@g.neec.ac.jp","k022g0066@g.neec.ac.jp",
+        "k022g0051@g.neec.ac.jp","k022g0111@g.neec.ac.jp","k022g0094@g.neec.ac.jp","k022g0128@g.neec.ac.jp",
+        "k022g0109@g.neec.ac.jp","k022g0124@g.neec.ac.jp","k022g0084@g.neec.ac.jp","k022g0082@g.neec.ac.jp",
+        "k022g0122@g.neec.ac.jp","k022g0061@g.neec.ac.jp","k022g0031@g.neec.ac.jp","k022g0076@g.neec.ac.jp",
+        "k022g0002@g.neec.ac.jp","k022g0090@g.neec.ac.jp","k022g0074@g.neec.ac.jp","k022g0012@g.neec.ac.jp",
+        "k022g0001@g.neec.ac.jp"
+    };
 
-    // 2019年
-    printf("【2019年】\n");
-    std::list<const char*> list2019;
-    for (int i = 0; i < 30; ++i) {
-        list2019.push_back(yamanote_1970[i][0]);
-    }
-    print_list(list2019);
+    // 学籍番号順にソート
+    std::sort(numbers.begin(), numbers.end());
 
-    // 2022年
-    printf("【2022年】\n");
-    std::list<const char*> list2022;
-    for (int i = 0; i < 30; ++i) {
-        // 高輪ゲートウェイ駅を品川と田町の間に挿入
-        list2022.push_back(yamanote_1970[i][0]);
-        if (strcmp(yamanote_1970[i][0], "Tamachi") == 0) {
-            list2022.push_back(takanawa);
-        }
+    // 表示
+    for (size_t i = 0; i < numbers.size(); ++i) {
+        printf("%s\n", numbers[i].c_str());
     }
-    print_list(list2022);
 
     return 0;
 }
